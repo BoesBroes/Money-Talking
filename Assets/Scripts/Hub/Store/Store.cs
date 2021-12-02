94ca74fec6df3463764af09a859ede37ef3f85b7
BoesBroes <jonathan.plasman@gmail.com>
2021-12-02 12:00:55 +0100

    UI stuff + gitignore what r u doin?

---
+++ Assets/Scripts/Hub/Store/Store.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Store : MonoBehaviour
+{
+    private GameObject[] storeType;
+
+
+    public void Start()
+    {
+        storeType = new GameObject[this.transform.childCount];
+
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            storeType[i] = this.transform.GetChild(i).gameObject;
+            storeType[i].SetActive(false);
+        }
+    }
+
+    public void ActivateCurrentStore(GameObject currentStore)
+    {
+        if(storeType.Contains(currentStore))
+        {
+            currentStore.SetActive(true);
+        }
+    }
+
+    public void DeactivateStores()
+    {
+        for (int i = 0; i < storeType.Length; i++)
+        {
+            storeType[i].SetActive(false);
+        }
+    }
+}
